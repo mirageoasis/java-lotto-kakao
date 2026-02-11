e086163ed9c6252e52a2e594ca3822a82a9a8835
mirageoasis <kimhw0820@naver.com>
2026-02-12 00:20:15 +0900

    refactor: 캐시 추가 및 생성자 private 전환

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,17 +1,35 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class LottoNumber {
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 45;
+	private static final List<LottoNumber> CACHE = createCache();
+
 	private final int value;
 
-	public LottoNumber(int value) {
-		validateNumber(value);
+	private LottoNumber(int value) {
 		this.value = value;
 	}
 
-	private void validateNumber(int value){
-		if (!(1 <= value && value <= 45))
+	public static LottoNumber of(int value) {
+		validateNumber(value);
+		return CACHE.get(value - MIN_NUMBER);
+	}
+
+	private static List<LottoNumber> createCache() {
+		List<LottoNumber> cache = new ArrayList<>();
+		for (int number = MIN_NUMBER; number <= MAX_NUMBER; number++) {
+			cache.add(new LottoNumber(number));
+		}
+		return List.copyOf(cache);
+	}
+
+	private static void validateNumber(int value){
+		if (!(MIN_NUMBER <= value && value <= MAX_NUMBER))
 			throw new IllegalArgumentException();
 	}
 

1b3453414870404036f3ac03e7cdfb6286d708c1
mirageoasis <kimhw0820@naver.com>
2026-02-12 01:40:58 +0900

    test: LottoTicket contains 메서드 테스트 추가

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -25,5 +25,26 @@ public class LottoTicketTest {
 		Assertions.assertThat(setNumbers.size()).isEqualTo(6);
 	}
 
+	@Test
+	@DisplayName("로또 티켓에 포함된 번호면 true를 반환한다")
+	void contains_true_when_number_exists() {
+		Set<LottoNumber> inputNumbers = IntStream.rangeClosed(1, 6)
+			.mapToObj(LottoNumber::of)
+			.collect(Collectors.toSet());
+		LottoTicket lottoTicket = new LottoTicket(inputNumbers);
+
+		Assertions.assertThat(lottoTicket.contains(LottoNumber.of(3))).isTrue();
+	}
+
+	@Test
+	@DisplayName("로또 티켓에 없는 번호면 false를 반환한다")
+	void contains_false_when_number_not_exists() {
+		Set<LottoNumber> inputNumbers = IntStream.rangeClosed(1, 6)
+			.mapToObj(LottoNumber::of)
+			.collect(Collectors.toSet());
+		LottoTicket lottoTicket = new LottoTicket(inputNumbers);
+
+		Assertions.assertThat(lottoTicket.contains(LottoNumber.of(7))).isFalse();
+	}
 
 }

4e516479d3d19d81f1fb17fe62fadd0708f675d3
mirageoasis <kimhw0820@naver.com>
2026-02-12 01:15:42 +0900

    refactor: LottoTicket 생성 책임을 Generator/Picker로 분리

--- src/main/java/lotto/Const.java
+++ src/main/java/lotto/Const.java
@@ -2,4 +2,5 @@ package lotto;
 
 public class Const {
 	static final int TICKET_PRICE = 1000;
+	static final int LOTTO_NUMBER_COUNT = 6;
 }
--- src/main/java/lotto/LottoTicket.java
+++ src/main/java/lotto/LottoTicket.java
@@ -1,18 +1,11 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class LottoTicket {
-
 	private final Set<LottoNumber> numbers;
-
-	public LottoTicket() {
-		this(generateUniqueSixNumbers());
-	}
 
 	public LottoTicket(Set<LottoNumber> numbers) {
 		validateNumbers(numbers);
@@ -24,20 +17,7 @@ public class LottoTicket {
 	}
 
 	private void validateNumbers(Set<LottoNumber> numbers) {
-		if (numbers.size() != 6)
-			throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
-	}
-
-	private static Set<LottoNumber> generateUniqueSixNumbers() {
-		List<Integer> numbers = new ArrayList<>();
-		for (int i = 1; i <= 45; i++) {
-			numbers.add(i);
-		}
-		Collections.shuffle(numbers);
-		Set<LottoNumber> lottoNumbers = new HashSet<>();
-		for (Integer number : numbers.subList(0, 6)) {
-			lottoNumbers.add(LottoNumber.of(number));
-		}
-		return lottoNumbers;
+		if (numbers.size() != Const.LOTTO_NUMBER_COUNT)
+			throw new IllegalArgumentException("로또 번호는 " + Const.LOTTO_NUMBER_COUNT + "개여야 합니다.");
 	}
 }

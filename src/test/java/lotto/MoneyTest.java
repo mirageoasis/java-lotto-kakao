dd894d90856bb3d98533d6d385b78b3ac1c6f9e6
mirageoasis <kimhw0820@naver.com>
2026-02-11 22:56:11 +0900

    test: 티켓 가격 이하로 입력하면 에러를 출력하도록 변경

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +11,7 @@ public class MoneyTest {
 	@DisplayName("구매 가능한 티켓 수량을 반환한다 (1,000의 배수)")
 	public void getTicketCount_when_amount_is_multiple_of_1000(){
 		int amount = 22000;
-		int answerCnt = amount / 1000;
+		int answerCnt = amount / Const.TICKET_PRICE;
 
 		Money money = new Money(amount);
 		Assertions.assertThat(money.toPurchaseCount()).isEqualTo(
@@ -21,11 +23,20 @@ public class MoneyTest {
 	@DisplayName("구매 가능한 티켓 수량을 반환한다 (1,000의 배수가 아닌 금액)")
 	public void getTicketCount_when_amount_has_remainder(){
 		int amount = 22500;
-		int answerCnt = amount / 1000;
+		int answerCnt = amount / Const.TICKET_PRICE;
 
 		Money money = new Money(amount);
 		Assertions.assertThat(money.toPurchaseCount()).isEqualTo(
 			answerCnt
 		);
+	}
+
+	@Test
+	@DisplayName("지불할 금액이 TICKET_PRICE원 미만인 경우")
+	public void error_ticket_price(){
+		int amount = Const.TICKET_PRICE - 1;
+
+		assertThatThrownBy(() -> new Money(amount))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 }

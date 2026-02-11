7b8918f83de731082be891cb4e168b90014e1ad1
mirageoasis <kimhw0820@naver.com>
2026-02-12 00:03:24 +0900

    refactor: 입력에 따른 테스트 추가

--- src/test/java/lotto/InputViewTest.java
+++ src/test/java/lotto/InputViewTest.java
@@ -1,4 +1,67 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 class InputViewTest {
-  
+
+	// 사용자의 입력을 시뮬레이션하는 편의 메서드
+	private InputView inputView(String input) {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+		return new InputView(new Scanner(in));
+	}
+
+	@DisplayName("당첨 번호 6개를 정상적으로 입력받는다.")
+	@Test
+	void readWinningNumbersSuccess() {
+		// given
+		InputView inputView = inputView("1, 2, 3, 4, 5, 6");
+
+		// when
+		WinningNumbers winningNumbers = inputView.readWinningNumbers();
+
+		// then
+		assertThat(winningNumbers).isNotNull();
+	}
+
+	@DisplayName("당첨 번호가 6개가 아니면 예외가 발생한다.")
+	@Test
+	void readWinningNumbersSizeError() {
+		// given
+		InputView inputView = inputView("1, 2, 3, 4, 5"); // 5개만 입력
+
+		// when & then
+		assertThatThrownBy(() -> inputView.readWinningNumbers())
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("당첨 번호는 6개를 입력해야 합니다.");
+	}
+
+	@DisplayName("당첨 번호에 중복이 있으면 예외가 발생한다.")
+	@Test
+	void readWinningNumbersDuplicateError() {
+		// given
+		InputView inputView = inputView("1, 2, 3, 4, 5, 5"); // '5' 중복
+
+		// when & then
+		assertThatThrownBy(() -> inputView.readWinningNumbers())
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("당첨 번호는 중복될 수 없습니다.");
+	}
+
+	@DisplayName("당첨 번호에 숫자가 아닌 값이 들어오면 예외가 발생한다.")
+	@Test
+	void readWinningNumbersNotIntegerError() {
+		// given
+		InputView inputView = inputView("1, 2, 3, a, 5, 6"); // 'a' 포함
+
+		// when & then
+		assertThatThrownBy(() -> inputView.readWinningNumbers())
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("숫자만 입력할 수 있습니다.");
+	}
 }
